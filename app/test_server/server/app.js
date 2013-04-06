2587b0cb5169d1f4cf61d3ce6e7d09bbf3ac88b7
Matt Kneiser <mattman@umich.edu>
2013-04-06 17:01:01 -0400

    Beginning the server stream handling logic

--- app/test_server/server/app.js
+++ app/test_server/server/app.js
@@ -9,16 +9,6 @@ var express = require('express')
   , child
 ;
 
-function puts(err, stdout, stderr){
-  sys.puts(err);
-  sys.puts(stdout);
-  sys.puts(stderr);
-}
-//child = exec("while [ true ]; do ls -la; done", puts);
-//child = exec("ls -la", puts);
-//child = exec("pwd", puts);
-child = exec("/opt/local/bin/python2.7 ~/Development/face/magnum/recog.py", puts);
-console.log(child.pid)
 //setInterval(function(){child = exec("ps", puts);}, 5000);
 
 // setup here
@@ -29,7 +19,8 @@ config(app);
 // define API routes here
 // ---------------------------------------------------------- //
 // GET
-app.get('/', router.index);
+app.get('/',                    router.index);
+app.get('/stream/:stream_name', router.stream)
 // ---------------------------------------------------------- //
 // ---------------------------------------------------------- //
 
--- app/test_server/server/router.js
+++ app/test_server/server/router.js
@@ -1,44 +1,25 @@
-/*var mongo = require('./database.js')
-  , email = require('./email.js');
+var sys     = require('sys')
+  , exec    = require('child_process').exec
+  , child
+  , streams = []
+;
 
-/*mongo.connect(function(msg) {
-  if(msg == null)
-    console.log("Mongo Connected!");
-  else 
-    console.log(msg);
-});
+function puts(err, stdout, stderr){
+  sys.puts(err);
+  sys.puts(stdout);
+  sys.puts(stderr);
+}
 
-// admin page
-exports.admin = function(req, res){
-  res.send("<h3> You must be and admin! </h3>");
-};
-
-// db test
-exports.db = function(req, res){
-  mongo.db.collection("test", function(err, collection){
-    collection.insert({ msg: "hello world" }, function(err, docs){
-      if(err) throw err
-      res.send(docs);
-    });
-  })
-};
-
-*/
 // main page
 exports.index = function(req, res){
   console.log('GET /');
   res.render('index', { title: 'Matt Kneiser' });
 };
-/*
-// email test
-exports.email = function(req, res){
-  email.send({ 
-    name: "Matt", 
-    email: "mattman@umich.edu"
-  } 
-  // templates defined in /server/email/
-  ,'template.jade', function(msg) { 
-    console.log(msg);
-    res.send(msg);
-  });
-};*/
+
+exports.stream = function(req, res){
+  child = exec("/opt/local/bin/python2.7 ~/Development/face/magnum/recog.py", puts);
+  streams[req.params.stream_name] = child.pid;
+  console.log(streams)
+
+  res.render('index', { 'title': child.pid });
+};
