ab6238be81b886f9ae3f9947b3daaf8ba76f3d25
Matt Kneiser <mattman@umich.edu>
2013-04-08 03:10:59 -0400

    Last changes from last night

--- app/test_server/server/router.js
+++ app/test_server/server/router.js
@@ -4,12 +4,6 @@ var sys     = require('sys')
   , streams = []
 ;
 
-function puts(err, stdout, stderr){
-  sys.puts(err);
-  sys.puts(stdout);
-  sys.puts(stderr);
-}
-
 // main page
 exports.index = function(req, res){
   console.log('GET /');
@@ -21,20 +15,23 @@ exports.stream = function(req, res){
     console.log(req.body);
     console.log('ip =', req.body['ip']);
     console.log('port =', req.body['port']);
-    //console.log(req.connection.remoteAddress)
-    var name = req.params.name || req.params.stream_name
+    var name = req.params.name || req.params.stream_name;
     if(name){
       console.log('stream');
       console.log(streams[name]);
+
+      // Only set up a connection if it 
       if(!streams[name]){
-        console.log('not streams[name]')
+        console.log('not streams[name]');
         streams[name] = req.body;
         var cmd = "/opt/local/bin/python2.7 ~/Development/face/magnum/recog.py";
         cmd += " rtsp://" + req.body['ip'] + ':' + req.body['port'];
-        console.log('[', cmd, ']')
-        child = exec(cmd , puts);
+        console.log('[', cmd, ']');
+        child = exec(cmd , function(err, stdout, stderr){
+          console.log('name', name);
+          delete streams[name];
+        });
         streams[name].pid = child.pid;
-        console.log(streams)
       }
     }else{
       console.log('Bad request: no stream name given')
@@ -53,3 +50,8 @@ exports.register = function(req, res){
 exports.trigger = function(req, res){
   console.log('TRIGGER FOUND -', req.params.id);
 };
+
+// Mux the stream for live viewing online
+// email the img to the user's email
+// account system? (config file, ppl auth at the web server and the camera is trusting)
+// assume ppl use this on the local network
